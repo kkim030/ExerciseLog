54823e376051a23ba7bfd928bf0c6716d349f8e2
Kelly Kim <kk030@students.cs.ubc.ca>
2022-02-11 15:32:12 -0800

    Phase 1 - FINAL updated tests

--- src/test/model/ProfileTest.java
+++ src/test/model/ProfileTest.java
@@ -16,16 +16,11 @@ class ProfileTest {
     @BeforeEach
     public void setUp() {
         mainProfile = new Profile("Kelly Kim", 18, 120);
-        //DayLog(ExerciseType type, int day, int month, int year, String notes, int logNumber)
+
         log1 = new DayLog(ExerciseType.PUSH, 1, 3, 2022, "new record 198ibs leg press", 3);
         log2 = new DayLog(ExerciseType.PULL, 2, 10, 2021, "Very tired today - weak performance", 99);
         log3 = new DayLog(ExerciseType.GLUTES, 2, 10, 2021, "Very tired today - weak performance", 4);
         log4 = new DayLog(ExerciseType.LEG, 21, 12, 2021, "Best workout ever", 19);
-
-        mainProfile.addExerciseLog(log1);
-        mainProfile.addExerciseLog(log2);
-        mainProfile.addExerciseLog(log3);
-        mainProfile.addExerciseLog(log4);
     }
 
     @Test
@@ -33,26 +28,46 @@ class ProfileTest {
         assertEquals("Kelly Kim", mainProfile.getName());
         assertEquals(18, mainProfile.getAge());
         assertEquals(120, mainProfile.getWeight());
-        assertEquals(4, mainProfile.getExerciseLog().size());
+        assertEquals(0, mainProfile.getExerciseLog().size());
     }
 
 
     @Test
-    public void testAddExerciseLog() {
-        assertEquals(4, mainProfile.getExerciseLog().size());
-        DayLog testDayLog = new DayLog(ExerciseType.PUSH, 2, 12, 2021, "NEW DAY", 13);
-        mainProfile.addExerciseLog(testDayLog);
-        assertEquals(5, mainProfile.getExerciseLog().size());
-        assertTrue(mainProfile.getExerciseLog().contains(testDayLog));
-    }
-
-    @Test
-    public void testRemoveLog() {
+    public void testAddOneExerciseLog() {
+        assertEquals(0, mainProfile.getExerciseLog().size());
+        mainProfile.addExerciseLog(log1);
+        assertEquals(1, mainProfile.getExerciseLog().size());
         assertTrue(mainProfile.getExerciseLog().contains(log1));
-        assertEquals(4, mainProfile.getExerciseLog().size());
-        assertTrue(mainProfile.removeLog(3));
+    }
+
+    @Test
+    public void testAddMultipleExerciseLogs() {
+        assertEquals(0, mainProfile.getExerciseLog().size());
+        log1 = new DayLog(ExerciseType.PUSH, 1, 3, 2022, "new record 198ibs leg press", 3);
+        log2 = new DayLog(ExerciseType.PULL, 2, 10, 2021, "Very tired today - weak performance", 99);
+        log3 = new DayLog(ExerciseType.GLUTES, 2, 10, 2021, "Very tired today - weak performance", 4);
+        mainProfile.addExerciseLog(log1);
+        mainProfile.addExerciseLog(log2);
+        mainProfile.addExerciseLog(log3);
         assertEquals(3, mainProfile.getExerciseLog().size());
+        assertTrue(mainProfile.getExerciseLog().contains(log1));
+        assertTrue(mainProfile.getExerciseLog().contains(log2));
+        assertTrue(mainProfile.getExerciseLog().contains(log3));
+    }
+
+    @Test
+    public void testAddThenRemoveLogsUntilNoLog() {
+        mainProfile.addExerciseLog(log1);
+        mainProfile.addExerciseLog(log2);
+        mainProfile.addExerciseLog(log3);
+        mainProfile.addExerciseLog(log4);
+
+        //removing a log number that is not part of the past logs
         assertFalse(mainProfile.removeLog(5));
+        assertEquals(4, mainProfile.getExerciseLog().size());
+
+        //removing log numbers part of the past logs
+        assertTrue(mainProfile.removeLog(3));
         assertEquals(3, mainProfile.getExerciseLog().size());
         assertTrue(mainProfile.removeLog(99));
         assertTrue(mainProfile.removeLog(4));
@@ -62,12 +77,17 @@ class ProfileTest {
     }
 
     @Test
-    public void testCountDayLog() {
-        assertEquals(4, mainProfile.countDayLog());
+    public void testAddRemoveLogsWhileCountingTotalDayLog() {
+        assertEquals(0, mainProfile.countDayLog());
         DayLog testDayLog = new DayLog(ExerciseType.PUSH, 2, 12, 2021, "NEW DAY", 13);
         mainProfile.addExerciseLog(testDayLog);
-        assertEquals(5, mainProfile.countDayLog());
+        assertEquals(1, mainProfile.countDayLog());
         mainProfile.removeLog(13);
+        assertEquals(0, mainProfile.countDayLog());
+        mainProfile.addExerciseLog(log1);
+        mainProfile.addExerciseLog(log2);
+        mainProfile.addExerciseLog(log3);
+        mainProfile.addExerciseLog(log4);
         assertEquals(4, mainProfile.countDayLog());
         mainProfile.removeLog(99);
         mainProfile.removeLog(4);
